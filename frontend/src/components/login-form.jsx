fca0b9ad34d770947e157965796937929e3906de
Matti <mag115@hi.is>
2024-10-15 20:21:06 +0200

    update git ignore

--- frontend/src/components/login-form.jsx
+++ frontend/src/components/login-form.jsx
@@ -70,7 +70,7 @@ const LoginForm = () => {
             <div className="flex items-center justify-between">
               <button
                 type="submit"
-                className="bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-opacity-50 w-full"
+                className="bg-black text-white px-4 py-2 rounded-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-opacity-50 w-full"
               >
                 Log In
               </button>
