1ad5eac077acf7b6c6d5e33300a8aff8052642ae
KolfinnaK <kolfinnakolbeinsdottir@gmail.com>
2024-10-18 15:23:50 +0000

    Mytasks, myinfo pages

---
+++ frontend/src/pages/mytasks.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Page } from '../components/page';
+import { MyTasks } from '../components/my-tasks'; // Import the component
+
+const MyTasksPage = () => {
+  return (
+    <Page>
+      <MyTasks />
+    </Page>
+  );
+};
+
+export { MyTasksPage };
