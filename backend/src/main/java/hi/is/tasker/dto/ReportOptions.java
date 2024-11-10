50368cfe6d4c4bf2cf4d52ca761c9520431ef5b8
olimarel <olimarel@gmail.com>
2024-11-10 19:29:45 +0000

    Implement backend functionality for customized report

---
+++ backend/src/main/java/hi/is/tasker/dto/ReportOptions.java
@@ -0,0 +1,32 @@
+package hi.is.tasker.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.Accessors;
+
+@Getter
+@Setter
+@Accessors(chain = true)
+@NoArgsConstructor
+@AllArgsConstructor
+public class ReportOptions {
+    private boolean includeTasks;
+    private boolean includeTimeSpent;
+    private boolean includePerformance;
+    // Add more fields as needed
+
+    // Getters and Setters
+    public boolean isIncludeTasks() { return includeTasks; }
+    public void setIncludeTasks(boolean includeTasks) { this.includeTasks = includeTasks; }
+
+    public boolean isIncludeTimeSpent() { return includeTimeSpent; }
+    public void setIncludeTimeSpent(boolean includeTimeSpent) { this.includeTimeSpent = includeTimeSpent; }
+
+    public boolean isIncludePerformance() { return includePerformance; }
+    public void setIncludePerformance(boolean includePerformance) { this.includePerformance = includePerformance; }
+
+    // ... Additional getters and setters
+}
+
