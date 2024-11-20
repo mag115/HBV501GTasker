7b223e5fb596a7c7be20c004dc8332ccb4b5142e
Matti <mag115@hi.is>
2024-11-20 16:30:45 +0000

    react url change

--- frontend/src/api/http.js
+++ frontend/src/api/http.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-axios.defaults.baseURL = 'http://localhost:8080';
+axios.defaults.baseURL = 'https://hbv501gtasker-1.onrender.com';
 axios.defaults.headers.post['Content-Type'] = 'application/json';
 
 axios.interceptors.request.use(
